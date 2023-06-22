7a3db2723d658d0fc9869d96da68242ee34a3996
eligijuslinkevicius <eligijuslinkev@gmail.com>
2023-06-22 14:02:06 +0300

    first commit and added html and styles

---
+++ app.js
@@ -0,0 +1,18 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+	this.info = function () {
+		if (this.read) return `${this.title} by ${this.author}, ${this.pages} pages, already read`;
+		else return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
+	};
+}
+
+function addBookToLibrary() {
+	myLibrary.push(new Book("The Hobbit", "J.R.R. Tolkien", 295, false));
+}
+
+addBookToLibrary();
